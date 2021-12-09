fb090590a5ef24014b508834524ff6e589bba4df
jeon <msjulyone@gmail.com>
2021-12-09 18:24:21 +0900

    add

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,19 @@ import React, { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
 import ItemList from "../src/component/ItemList";
-import { Divider, Header } from "semantic-ui-react";
+import { Divider, Header, Loader } from "semantic-ui-react";
 
 export default function Home() {
   const [list, setList] = useState([]);
-  const API_URL =
-    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+  const [isLoading, setIsLoading] = useState(true);
+
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
   function getData() {
-    axios.get(API_URL).then((res) => setList(res.data));
+    axios.get(API_URL).then((res) => {
+      setList(res.data);
+      setIsLoading(false);
+    });
   }
 
   useEffect(() => {
@@ -24,16 +28,29 @@ export default function Home() {
       <Head>
         <title>HOME | JEON</title>
       </Head>
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        Best Items
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        New Items
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(9)} />
+
+      {isLoading && (
+        <div style={{ padding: "300px 0" }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+
+      {!isLoading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            Best Items
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            New Items
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
     </div>
   );
 }
